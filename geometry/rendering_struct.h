757559e2935d7673bbc4de72daf636c240d4072f
KimximyaFan <107273680+KimximyaFan@users.noreply.github.com>
2025-09-08 05:42:22 +0900

    refactor: rename to rendering struct

---
+++ geometry/rendering_struct.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "material/color.h"
+
+struct Projected_Vertex
+{
+    float x, y, z, invW;
+    float r_over_w, g_over_w, b_over_w, u_over_w, v_over_w;
+};
+
+struct Projected_Triangle
+{
+    Projected_Vertex v0, v1, v2;
+};
+
+struct Triangle_Reference
+{
+    int tid, index;
+    int x0, y0, x1, y1;
+};
+
+struct Bounding_Box
+{
+    int x0, y0, x1, y1;
+};
+
+struct Tile
+{
+    int x0, y0, x1, y1;
+    int id;
+};
